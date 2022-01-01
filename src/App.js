31f6678b21dd8e6a1d60d0896879382845d23451
Pooja Soliwal <poojasoliwal2222@gmail.com>
2022-01-01 18:22:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Image, Center, Button, HStack } from "@chakra-ui/react";
+import { Image, Center, Button } from "@chakra-ui/react";
 import Layout from "./layout/Layout";
 import Img from "./assets/img.jpg";
 import { Typewriter } from "react-simple-typewriter";
@@ -6,32 +6,21 @@ import ProjectWork from "./components/projectWork";
 import About from "./components/About";
 import { BrowserRouter, Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import Lottie from "react-lottie";
-import BubblesAnimation from "./Lotttie/87280-bubbles-lottie-animation.json";
+import "./index.css";
 
 function App() {
-  const button = { fontSize: "1rem", backgroundColor: "green", color: "white" };
-  const Bubbles = {
-    loop: true,
-    autoplay: true,
-    animationData: BubblesAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+  const cv = { fontSize: "1rem", backgroundColor: "green", color: "white" };
+  const about = { fontSize: "1rem", border: "solid 1px #008000", color: "green" };
+
   return (
     <>
       <BrowserRouter>
         <Layout />
-        <Center mt="2rem">
-          <HStack>
-            <Image src={Img} borderRadius='full' boxSize='16rem' />
-            <Lottie options={Bubbles} height={320}
-              width={455} />
-          </HStack>
+        <Center mt="4rem">
+          <Image src={Img} borderRadius='full' boxSize={{ sm: "12rem", md: "14rem", lg: "16rem" }} />
         </Center>
-        <h1 style={{ fontSize: "3rem", textAlign: "center", padding: "4rem" }}>I'm
-          <span style={{ color: "#008000", fontWeight: "bold" }}>
+        <h1 className="mediaQuery" style={{ textAlign: "center", padding: "4rem" }}>I'm
+          <span className="mediaQuery" style={{ color: "#008000", fontWeight: "bold" }}>
             <Typewriter
               loop
               cursor
@@ -45,10 +34,10 @@ function App() {
         </h1>
         <Center mb="8rem">
           <Link to="/Pooja-Soliwal-Resume.pdf" target="_blank" download>
-            <Button style={button}>Download CV</Button>
+            <Button style={cv}>Download CV</Button>
           </Link>
           <HashLink to="#about">
-            <Button style={button} ml="2rem">Know more</Button>
+            <Button style={about} ml="2rem">Know more</Button>
           </HashLink>
         </Center>
         <ProjectWork />
